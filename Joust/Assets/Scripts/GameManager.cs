aefac571c4882719f21a4339d9b2a054feadb49b
lsapoz <lyndonsapozhnik@gmail.com>
2016-02-22 20:55:51 -0600

    - Add a GameManager object and instantiate it as a singleton that persists between scenes - Create a scenes folder, add the MenuScene, and move the MainScene over - Create a simple title screen to MenyScene and add a button that loads MainScene - Add MenuScene and MainScene to the build settings

---
+++ Joust/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // singleton instance of the GameManager
+    public static GameManager instance = null;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        // manage the singleton instance of the GameManager
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+	
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
